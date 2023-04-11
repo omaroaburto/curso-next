909fce4b9ca2863373008fddcd8701905f7568e3
Omaro Aburto Leiva <oaburto.freelance@gmail.com>
2023-04-11 18:39:30 -0400

    curso de nextjs básico

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import styles from "@/styles/Home.module.css";
+ 
+export default function about() {
+    return (
+        <>
+            <h1>about</h1>
+            <Link href='/'>Inicio</Link>
+        </>
+    )
+}
